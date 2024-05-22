3249bafc217f096a9c764763fafae5c449fa735d
Albert Zaroyan <albertzaroyan@yandex.ru>
2024-05-22 22:09:06 +0300

    initial commit

---
+++ src/main/java/com/zaroyan/onlineWalletApp/OnlineWalletAppApplication.java
@@ -0,0 +1,13 @@
+package com.zaroyan.onlineWalletApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineWalletAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineWalletAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zaroyan/onlineWalletApp/OnlineWalletAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaroyan.onlineWalletApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineWalletAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
