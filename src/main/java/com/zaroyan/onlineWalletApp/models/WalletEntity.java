d4a15131902ba44694cf87ce840f4c44a05e98d5
Albert Zaroyan <albertzaroyan@yandex.ru>
2024-05-23 15:32:28 +0300

    Создание Entity-модели (Класс WalletEntity) и Тип перечисления (enum OperationType)

---
+++ src/main/java/com/zaroyan/onlineWalletApp/models/WalletEntity.java
@@ -0,0 +1,16 @@
+package com.zaroyan.onlineWalletApp.models;
+
+import com.zaroyan.onlineWalletApp.utils.OperationType;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+/**
+ * @author Zaroyan
+ */
+public class WalletEntity {
+    private UUID walletId;
+    private OperationType operationType;
+    private BigDecimal amount;
+
+}
---
+++ src/main/java/com/zaroyan/onlineWalletApp/utils/OperationType.java
@@ -0,0 +1,9 @@
+package com.zaroyan.onlineWalletApp.utils;
+
+/**
+ * @author Zaroyan
+ */
+public enum OperationType {
+    DEPOSIT,
+    WITHDRAW
+}
