e9808e6e395548078d281c619bfdd64b9af042fa
Albert Zaroyan <albertzaroyan@yandex.ru>
2024-05-23 15:44:40 +0300

    В WalletEntity применены анотации

--- src/main/java/com/zaroyan/onlineWalletApp/models/WalletEntity.java
+++ src/main/java/com/zaroyan/onlineWalletApp/models/WalletEntity.java
@@ -1,6 +1,8 @@
 package com.zaroyan.onlineWalletApp.models;
 
 import com.zaroyan.onlineWalletApp.utils.OperationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -8,7 +10,9 @@ import java.util.UUID;
 /**
  * @author Zaroyan
  */
+@Entity
 public class WalletEntity {
+    @Id
     private UUID walletId;
     private BigDecimal amount;
 
