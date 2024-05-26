f3b45d6accbc1508f731ebc6d5a6491413ecb868
Albert Zaroyan <albertzaroyan@yandex.ru>
2024-05-26 12:37:10 +0300

    Реализован метод findById с применением аннотации     @Lock(LockModeType.PESSIMISTIC_WRITE)

--- src/main/java/com/zaroyan/onlineWalletApp/repositories/WalletRepository.java
+++ src/main/java/com/zaroyan/onlineWalletApp/repositories/WalletRepository.java
@@ -1,9 +1,12 @@
 package com.zaroyan.onlineWalletApp.repositories;
 
 import com.zaroyan.onlineWalletApp.models.WalletEntity;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -11,4 +14,7 @@ import java.util.UUID;
  */
 @Repository
 public interface WalletRepository extends JpaRepository<WalletEntity, UUID> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<WalletEntity> findById(UUID uuid);
 }
